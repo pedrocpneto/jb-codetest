a4db7812161f55976a4b7834ef100c1b099502e2
Pedro Pereira Neto <pedrocpneto@gmail.com>
2021-10-30 12:49:41 -0300

    Adding reset, and retry for API instabilities/random errors

--- README.md
+++ README.md
@@ -5,8 +5,19 @@ This is a codetest from Juros Baixos which integrates with http://codetest.juros
 There will be in this repository two implementations, one Proof-of-concept script in Python located at `python-poc` directory, and a `springboot-java` implementation which, as name implies, is a Web Java implementation with Spring Framework.
 
 ## Python proof-of-concept usage
-Requires Python 3
+Requires Python 3 and Retry Package (https://pypi.org/project/retry/).
 
+To install Retry;
 ```shell
-JB_API_KEY=<your_key> ./poc-python/script.py
+$ pip install retry
+```
+
+To try getting the treasure:
+```shell
+$ JB_API_KEY=<your_key> ./poc-python/script.py
+```
+
+To reset challenge:
+```shell
+$ JB_API_KEY=<your_key> ./poc-python/script.py reset
 ```
--- poc-python/script.py
+++ poc-python/script.py
@@ -1,30 +1,60 @@
 #!/usr/bin/python3
 
+
 import os
 import requests
 import hashlib
 import json
+import sys
+
+from requests.models import HTTPError
+from retry import retry
 
 JB_URL = 'https://codetest.jurosbaixos.com.br/v1/fizzbuzz/'
 
-def get_numbers(headers):
-    return requests.get(JB_URL, headers=headers).json()
+def raise_not_ok(response, acceptable=[200]):
+    obj = response.json()
+    if response.status_code != 200:
+        # it seems 'No puzzle' is a valid response even in post and delete requests
+        if obj['errors'][0]['message'] != 'No puzzle':
+            raise HTTPError(f'{response.status_code} Error: {response.reason}', response=response)
+    elif ('errors' in obj):
+        raise HTTPError('200 response with errors')
 
+
+def reset(headers):
+    response = requests.get(f'{JB_URL}reset', headers=headers)
+    print(f'reset: {response.status_code}', response.text)
+
+@retry((HTTPError, ValueError))
+def get_numbers(headers):
+    response = requests.get(JB_URL, headers=headers)
+    print(f'get_numbers: {response.status_code}', response.text)
+    raise_not_ok(response)
+    obj = response.json()
+    return obj
+
+@retry((HTTPError, ValueError))
 def post_hash(headers, resolved_puzzle):
     hash = json_sha_256(resolved_puzzle)
     response = requests.post(f'{JB_URL}{hash}', json=resolved_puzzle, headers=headers)
-    print(response.text)
-    response.raise_for_status()
+    print(f'post_hash: {response.status_code}', response.text)
+    raise_not_ok(response)
     return hash
 
+@retry((HTTPError, ValueError))
 def delete_range(headers, hash):
     response = requests.delete(f'{JB_URL}{hash}', headers=headers)
-    print(response.text)
-    response.raise_for_status()
+    print(f'delete_range: {response.status_code}', response.text)
+    raise_not_ok(response)
 
+@retry((HTTPError, ValueError))
 def get_can_i_has_treasure(headers, hash):
-    request = requests.get(f'{JB_URL}{hash}/canihastreasure', headers=headers)
-    return request.ok, request.json()
+    response = requests.get(f'{JB_URL}{hash}/canihastreasure', headers=headers)
+    print(f'get_can_i_has_treasure: {response.status_code}', response.text)
+    raise_not_ok(response)
+    obj = response.json()
+    return response.status_code == 200 and 'treasure' in obj, obj
 
 def fizz_buzz(number):
     result = ''
@@ -47,16 +77,20 @@ def json_sha_256(obj):
 
 if __name__ == '__main__':
     jb_headers = {'X-API-KEY': get_api_key()}
+    
+    if len(sys.argv) > 1 and sys.argv[1] == 'reset':
+        reset(jb_headers)
+        exit(0)
+
     iteration = 1
     while True:
         print (f'iteration {iteration}')
         iteration+=1
         
         numbers = get_numbers(jb_headers)   
-        print(f'numbers: {numbers}')
         
         fb_numbers = list(map(fizz_buzz, numbers))
-        print(f'fb numbers: {fb_numbers[:10]} ...')
+        print(f'fb_numbers: {fb_numbers[:10]} ...')
         
         # print(f'SHA256: {json_sha_256(list(map(fizz_buzz, [3,5,15])))}') # c66a63862cf416c2acfe81ae697c066cff80b430af31fc9cae70957f355ded7d
         print(f'SHA256: {json_sha_256(fb_numbers)}')

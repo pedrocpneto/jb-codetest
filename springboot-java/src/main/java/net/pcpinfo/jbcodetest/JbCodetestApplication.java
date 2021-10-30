bd5a3639f334ff3ac8d4d4662e1a0677a1eb2a09
Pedro Pereira Neto <pedrocpneto@gmail.com>
2021-10-30 16:15:21 -0300

    spring boot project initialized

---
+++ springboot-java/src/main/java/net/pcpinfo/jbcodetest/JbCodetestApplication.java
@@ -0,0 +1,13 @@
+package net.pcpinfo.jbcodetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbCodetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbCodetestApplication.class, args);
+	}
+
+}
